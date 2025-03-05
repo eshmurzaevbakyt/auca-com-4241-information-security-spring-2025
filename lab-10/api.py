677dcc5ba734d9c5838f96bf25af2503eceb2811
Bakyt Eshmurzaev <eshmurzaev_b@auca.kg>
2025-03-05 23:54:20 +0600

    Completed lab-10

---
+++ lab-10/api.py
@@ -0,0 +1,7 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/api/', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello from Flask API!"})
+if __name__ == '__main__':
+    app.run(port=5000)
